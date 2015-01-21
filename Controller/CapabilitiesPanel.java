aca8531a4ea316e86f7e9f54c651d7ffbcc2d5e7
wtang2 <tang_jeen@hotmail.com>
2015-01-20 20:04:48 -0800

    Update CapabilitiesPanel.java

--- Controller/CapabilitiesPanel.java
+++ Controller/CapabilitiesPanel.java
@@ -1,9 +1,6 @@
 package Controller;
 
 import java.awt.Color;
-
-
-
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -23,18 +20,14 @@ import model.RMOS;
 import model.RCM;
 import model.Serialize_Deserialize;
 
-
 @SuppressWarnings("serial")
 public class CapabilitiesPanel extends JPanel implements ActionListener {
-
-	
 	private RMOS rmos;
 	private ArrayList<ArrayList<JTextField>> rows;
 	private RCM selectedRCM;
 	private int indexCounter;
 	
-	public CapabilitiesPanel(RMOS rmos) 
-	{
+	public CapabilitiesPanel(RMOS rmos) {
 		setLayout(new GridLayout(0,5));
 		this.setPreferredSize(new Dimension(600, 300));
 		this.rmos = rmos;
@@ -44,86 +37,68 @@ public class CapabilitiesPanel extends JPanel implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String str = (String) ((JComboBox)e.getSource()).getSelectedItem();
-		
 		if (str == null || str.equalsIgnoreCase("All")) 
 			return;
-		
 		RCM selectedRCM = rmos.getRCMByID(str);		  
-		
 		updatePanel(selectedRCM);
 	}
 
 	public void updatePanel(RCM selectedRCM){
-		
 		if (selectedRCM == this.selectedRCM)
 			return;
-
-		   this.removeAll();
-		   rows.clear();
-		   this.selectedRCM = selectedRCM;
-		   indexCounter = 0;
-
-		   
-		   JLabel t1 = new JLabel("Item type");
-		   t1.setForeground(Color.BLUE); 
-		   
-		   JLabel t2 = new JLabel("Price for");  
-		   t2.setForeground(Color.BLUE); 
-
-		   
-		   JLabel t3 = new JLabel("weight (lbs)");  
-		   t3.setForeground(Color.BLUE); 
-
-		  		   
-		   JButton newItem = new JButton("Add item type");
-//		   newItem.setPreferredSize(new Dimension(100, 100));
-		   newItem.enableInputMethods(true);
-		   newItem.addActionListener(new NewTypeListener());
-		   
-		   this.add(t1);
-		   this.add(t2); 
-		   this.add(t3); 
-
-		   this.add(new JLabel("")); 
-		   this.add(newItem);
-		   
-		 		   
-		   ArrayList<ItemType> list = selectedRCM.getTypeList();
-		  
-		   for (int i = 0; i < list.size(); i++)
-		   {
-			   addRow(list.get(i));
-		   }
-		   
-		   this.revalidate();
-		   this.repaint();
+		this.removeAll();
+		rows.clear();
+		this.selectedRCM = selectedRCM;
+		indexCounter = 0;
+		
+		JLabel t1 = new JLabel("Item type");
+		t1.setForeground(Color.BLUE); 
+		
+		JLabel t2 = new JLabel("Price for");  
+		t2.setForeground(Color.BLUE); 
+		
+		JLabel t3 = new JLabel("weight (lbs)");  
+		t3.setForeground(Color.BLUE); 
+		     
+		JButton newItem = new JButton("Add item type");
+		newItem.enableInputMethods(true);
+		newItem.addActionListener(new NewTypeListener());
+		
+		this.add(t1);
+		this.add(t2); 
+		this.add(t3); 
+		this.add(new JLabel("")); 
+		this.add(newItem);
+		    
+		ArrayList<ItemType> list = selectedRCM.getTypeList();
+		
+		for (int i = 0; i < list.size(); i++) {
+			addRow(list.get(i));
+		}
+		
+		this.revalidate();
+		this.repaint();
 	}
 
-	
-	
 	private void addRow(ItemType itemType) {
-
 		   JTextField nameField = new  JTextField(1);
-	    
 		   String name = "";
 		   double price = 0;
 		   double weight = 0;
 		   boolean isNew = itemType == null;
 		   
-		   if (!isNew)
-		   {
+		   if (!isNew) {
 			   name = itemType.getType();
 			   price = itemType.getPrice();
 			   weight = itemType.getWeightInlb();
 		   }
 		   
 		   nameField.setText("" + name);
-		    
 		   nameField.setHorizontalAlignment(SwingConstants.CENTER);
 		   nameField.setEditable(isNew);
 		   
-		   JTextField priceField = new  JTextField(1);
-		  		   
+		   JTextField priceField = new  JTextField(1); 
+		   
 		   priceField.setText("" + price); 
 		   priceField.setHorizontalAlignment(SwingConstants.CENTER);
 		   priceField.setEditable(isNew);
@@ -135,6 +110,7 @@ public class CapabilitiesPanel extends JPanel implements ActionListener {
 		   weightField.setEditable(isNew);		   
 		   
 		   ArrayList<JTextField> list = new ArrayList<JTextField>();
+		   
 		   rows.add(list);
 		   list.add(nameField);
 		   list.add(priceField);
@@ -157,25 +133,20 @@ public class CapabilitiesPanel extends JPanel implements ActionListener {
 		   indexCounter++;
 	}
 	
-	
 	private class ChangeListener implements ActionListener {
-
 		private ArrayList<JTextField> list;
-		
 		ChangeListener(ArrayList<JTextField> list) {
-			
 			this.list = list;
 		}
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			for (int i = 0; i < list.size(); i++){
+			for (int i = 0; i < list.size(); i++) {
 				list.get(i).setEditable(true);
 			}
 		}			
 	}
 	
 	private class NewTypeListener implements ActionListener {
-		
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			CapabilitiesPanel.this.addRow(null);
@@ -184,10 +155,8 @@ public class CapabilitiesPanel extends JPanel implements ActionListener {
 	}
 	
 	private class SaveListener implements ActionListener {
-		
 		private int index;
-		private boolean isNew;
-//		private JButton button;
+		private boolean isNew; 
 		
 		SaveListener(int index, boolean isNew) {
 			this.index = index;
@@ -195,11 +164,8 @@ public class CapabilitiesPanel extends JPanel implements ActionListener {
 		}
 		@Override
 		public void actionPerformed(ActionEvent e) {
-
 			ArrayList<JTextField> list = rows.get(index);
-
 			try {
-
 				String name = list.get(0).getText();
 
 				if (name.isEmpty())
@@ -217,11 +183,9 @@ public class CapabilitiesPanel extends JPanel implements ActionListener {
 				
 				JOptionPane.showMessageDialog(null,
 						" \nSuccess! Item Type was successfully saved.", null, JOptionPane.INFORMATION_MESSAGE);
-				
 				Serialize_Deserialize.serializeObject(rmos);
 				
-			} catch (NullPointerException exception) 
-			{
+			} catch (NullPointerException exception) {
 				exception.printStackTrace();
 				JOptionPane
 				.showMessageDialog(
@@ -229,7 +193,6 @@ public class CapabilitiesPanel extends JPanel implements ActionListener {
 						"Invalid input! Empty field",
 						"Error",
 						JOptionPane.ERROR_MESSAGE);
-				
 			} catch (NumberFormatException exception) {
 				JOptionPane
 				.showMessageDialog(
@@ -237,7 +200,6 @@ public class CapabilitiesPanel extends JPanel implements ActionListener {
 						"Invalid input! Please enter a positive number",
 						"Error",
 						JOptionPane.ERROR_MESSAGE);
-
 			} catch (IllegalArgumentException exception) {
 				JOptionPane
 				.showMessageDialog(
@@ -246,7 +208,6 @@ public class CapabilitiesPanel extends JPanel implements ActionListener {
 						"Error",
 						JOptionPane.ERROR_MESSAGE);
 			}
-			
 		}			
 	}
 }
