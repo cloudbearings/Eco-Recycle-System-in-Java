9ac9f24dd3ef33441058806a3af81e99dc718ebc
wtang2 <tang_jeen@hotmail.com>
2015-01-20 19:34:57 -0800

    change folder name
    folder name

--- Model/Coupon.java
+++ Model/Coupon.java
@@ -3,7 +3,6 @@ package model;
 /**
  * This class represents one reward (Coupon) that will be given to customer in the case that
  * there is not enough money in the RCM.
- * 
  * @author  Woon Jeen Tang, Yuanyuan Xie
  * @studentID W0987907, W0984770	
  * @Section: Tuesday and Thursday 
