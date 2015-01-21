7f360cb96484dd020557b1ce9cea38b0bb69c1c3
wtang2 <tang_jeen@hotmail.com>
2015-01-20 19:47:17 -0800

    Update Project.java

--- main/Project.java
+++ main/Project.java
@@ -1,37 +1,17 @@
 package main;
 
-//import java.awt.Container;
-//
-//import java.awt.GridLayout;
-//
-//import javax.swing.JFrame;
-//import javax.swing.JLabel;
-//import javax.swing.JPanel;
-
 import Controller.AdminLogin;
 import Controller.CustomerInterface;
 import model.RMOS;
 import model.Serialize_Deserialize;
 
 public class Project {
-
-	 	
-	public static void main(String[] args) 
-    {
+	public static void main(String[] args) {
 		RMOS RMOS1 = Serialize_Deserialize.deserializeRMOS();
-
 		if (RMOS1 == null)
 			RMOS1 = new RMOS();
-
-    	
-    	AdminLogin si = new AdminLogin(RMOS1);
-    	si.login();
-
-    	
-    	new CustomerInterface(RMOS1);
-//    	CustomerInterface customer2 = new CustomerInterface(RMOS1);
-    	
-	
-    }
-    
+	    	AdminLogin si = new AdminLogin(RMOS1);
+	    	si.login();
+	    	new CustomerInterface(RMOS1);
+    	}
 }
